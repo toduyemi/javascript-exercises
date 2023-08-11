97bb23f7ac661ca153c1ae1269cf8ceed2310c72
toduyemi <temit96@hotmail.com>
2023-08-10 23:28:43 -0400

    complete reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let charArray = string.split('')
+    let output = '';
+    
+    for (let i = charArray.length - 1; i >= 0; i--) {
+        output += `${charArray[i]}`;
+    }
+    
+    return output;
+    
 };
 
 // Do not edit below this line
