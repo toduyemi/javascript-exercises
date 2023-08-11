57600a9449ba13e60cffd1856d45549696b8ccb5
toduyemi <temit96@hotmail.com>
2023-08-11 02:37:01 -0400

    sum all numbers in range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (number1 < 0 || number2 < 0 || typeof number1 !== 'number' || typeof number2 !== 'number') {
+        return 'ERROR';
+    }
+    let sum = 0;
+    for (let i = Math.min(number1, number2); i <= Math.max(number1, number2); i++) {
+        sum += i;
+    }
+    return sum;
 
-};
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
