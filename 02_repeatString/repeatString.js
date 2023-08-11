835999ef0f746556dc8bc98b4a7ddd4c0b3ed354
toduyemi <temit96@hotmail.com>
2023-08-10 21:38:16 -0400

    complete repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let output = '';
+    for (let i = 0; i < number; i++) {
+        output += string;
+    }
 
+    if (number < 0) {
+        return 'ERROR';
+    }
+
+    if (output || output == '') {
+        return output;
+    }
+    
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
