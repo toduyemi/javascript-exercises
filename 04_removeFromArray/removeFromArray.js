974f39c62997d438911f91d5d5af3aff03272f20
toduyemi <temit96@hotmail.com>
2023-08-11 00:57:52 -0400

    create function that removes specific element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === arguments[i]) {
+                array.splice(j, 1);
+                break;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
